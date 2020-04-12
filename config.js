cb848cb8d1667f5996c727b178d2f5aa0c73953e
YoheiUmezu <ymeyouhei@gmail.com>
2020-04-12 18:58:03 +0900

    first-commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://Yohei:707797Tully@cluster0-dcz9g.mongodb.net/test?retryWrites=true&w=majority',
+    // 'mongodb+srv://classsed:6lxxxlexaTslHLPe@cluster0-pcsru.mongodb.net/merng?retryWrites=true',
+  SECRET_KEY: 'some very secret key'
+};
