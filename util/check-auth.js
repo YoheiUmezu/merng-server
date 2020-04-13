ef4ba5509bc29219f18b485c834b9616953bb48d
YoheiUmezu <ymeyouhei@gmail.com>
2020-04-13 22:09:28 +0900

    rererecommit

--- util/check-auth.js
+++ util/check-auth.js
@@ -4,12 +4,12 @@ const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = require('../config');
 
 module.exports = (context) => {
-  context = { ... headers }
+  //context = { ... headers }
   
   const authHeader = context.req.headers.authorization;
   if (authHeader) {
     // Bearer ....
-    const token = authHeader.split('Bearer ')[1];
+    const token = req.headers.authorization || '';
     if (token) {
       try {
         const user = jwt.verify(token, SECRET_KEY);
